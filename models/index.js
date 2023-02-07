0dd00fe315f195efbbce079591f7f10004193de0
wdiste <Wdiste49@gmail.com>
2023-02-07 05:29:10 -0500

    basic structure added based on previous examples. config, controllers, db, models complete.  views and utils added based on class lessons.  working on seeds

---
+++ models/index.js
@@ -0,0 +1,24 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Post');
+
+
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'CASCADE'
+});
+
+module.exports = { User, Post, Comment };
